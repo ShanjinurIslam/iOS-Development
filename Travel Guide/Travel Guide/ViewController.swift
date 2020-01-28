048ee6d4f3f02f400c7fa5c97fe60149580b6208
Shanjinur Islam <spondoncsebuet@gmail.com>
2020-01-29 00:48:09 +0600

    travel_guide_figma_to_ios

---
+++ Travel Guide/Travel Guide/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Travel Guide
+//
+//  Created by Shanjinur Islam on 1/28/20.
+//  Copyright © 2020 Shanjinur Islam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func attractionsButton(_ sender: Any) {
+        print("I am pressed");
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
